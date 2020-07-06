9c62a527652104510f28e644b3962a77d7e81470
Ham Adams <HamAdams@disroot.org>
2020-07-05 22:02:07 -0400

    added some randomized messages

--- src/components/Morning.vue
+++ src/components/Morning.vue
@@ -1,9 +1,22 @@
 <template>
-  <div>It's time for morning beans!</div>
+  <div>{{ message }}</div>
 </template>
 
 <script>
+const messages = [
+  "Rise and shine! It's time for coffee!",
+  "Nothing like a cup of joe to start your morning off right",
+  "It's going to be a good day, as long as you start off with some coffee",
+  "Wakey wakey, time for morning beans!",
+];
+
 export default {
   name: "Morning",
+  data: () => {
+    return {
+      name: "Morning",
+      message: messages[Math.floor(Math.random() * messages.length)],
+    };
+  },
 };
 </script>
