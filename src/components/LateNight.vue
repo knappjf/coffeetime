4fed415e2318a8ef97eadcd279d1e62d01fbbdd7
Ham Adams <HamAdams@disroot.org>
2020-07-07 12:19:00 -0400

    fixed a typo

--- src/components/LateNight.vue
+++ src/components/LateNight.vue
@@ -6,7 +6,7 @@
 const messages = [
   "It's time for late night beans",
   "Boy you're up late, better put on another pot of coffee",
-  "Boss got you working late? Somee late night beans should make it better",
+  "Boss got you working late? Some late night beans should make it better",
   "You've made it this far, might as well have another cup of joe to get through the night",
 ];
 
